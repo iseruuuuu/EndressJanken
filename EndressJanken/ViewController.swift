29369d71e7209fdbda804dab3f193e3fe934fb7a
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-01-30 15:15:37 +0900

    Initial Commit

---
+++ EndressJanken/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EndressJanken
+//
+//  Created by 井関竜太郎 on 2021/01/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
